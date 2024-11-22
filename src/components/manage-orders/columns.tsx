973a3fc9b4a98e8adff178041fa83e6d92780489
phuccoderr <phuctapcode@gmail.com>
2024-11-22 13:36:48 +0700

    Rebuild

--- src/components/manage-orders/columns.tsx
+++ src/components/manage-orders/columns.tsx
@@ -1,3 +1,4 @@
+import { OrderItems } from "@/types/order-item.type";
 import { Orders } from "@/types/order.type";
 import { ColumnDef } from "@tanstack/react-table";
 
@@ -10,34 +11,53 @@ export const columns: ColumnDef<Orders>[] = [
     },
   },
   {
-    accessorKey: "managed_by",
+    accessorKey: "",
     header: "Email",
     cell: ({ row }) => {
-      const managed = row.getValue("managed_by");
+      const managed = row.getValue("user");
       return <h1>{managed?.email ?? "unknown"}</h1>;
     },
   },
   {
-    accessorKey: "buyer_name",
+    accessorKey: "",
     header: "Name",
     cell: ({ row }) => {
-      const managed = row.getValue("managed_by");
+      const managed = row.getValue("user");
       return <h1>{managed?.name ?? "unknown"}</h1>;
     },
   },
   {
-    accessorKey: "buyer_phone",
+    accessorKey: "",
     header: "Phone",
     cell: ({ row }) => {
-      const managed = row.getValue("managed_by");
+      const managed = row.getValue("user");
       return <h1>{managed?.phone_number ?? "unknown"}</h1>;
     },
   },
+  {
+    accessorKey: "",
+    header: "Address",
+    cell: ({ row }) => {
+      const managed = row.getValue("franchise");
+      return <h1>{managed?.address ?? "unknown"}</h1>;
+    },
+  },
+
   {
     accessorKey: "created_at",
     header: "Created At",
     cell: ({ row }) => {
       return row.getValue("created_at");
+    },
+  },
+  {
+    accessorKey: "",
+    header: "Total",
+    cell: ({ row }) => {
+      const total = row.getValue("orderItems") as unknown as OrderItems[];
+      return total.reduce((acc, item) => {
+        return acc + item.price * item.quantity;
+      }, 0);
     },
   },
 ];
